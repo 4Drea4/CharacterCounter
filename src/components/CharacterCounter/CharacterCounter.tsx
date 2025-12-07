ce18be129a8262be1eb27334420fa645a6cffa9a
4drea4 <bakerand@outlook.com>
2025-12-06 18:01:55 -0600

    creating the character count file, a bit confused even after watching videos and reading documentation, I am going to step away from the computer and comeback

--- src/components/CharacterCounter/CharacterCounter.tsx
+++ src/components/CharacterCounter/CharacterCounter.tsx
@@ -2,16 +2,37 @@
 import React, {useState} from 'react';
 import type { TextInput} from '../TextInput.tsx';
 import type { StatsDisplay } from '../CharacterCounter.tsx';
+import type { CharacterCounterProps } from '../../types/index.ts';
 
 //create two use states one for text input and one for character count
 
 
 
-export default count {
-const [text ,textInput] = useState(0);
+export default const CharacterCounter: React.FC<CharacterCounterProps> =  ({
 
+const [text ,setInput] = useState("");
 // pass function instead of object
+}) => {
+    return (
+    <div>
+    <h2> Character Count</h2>
+    
+    // text input
+        //event on type
+    
+    //stats here
+        //event on type
+
+
+    </div>
+    )
+
+    //reset stats when empty
+
 }
-export default stats {
-    const [stats,StatsDisplay]
-}
+
+
+
+
+
+
