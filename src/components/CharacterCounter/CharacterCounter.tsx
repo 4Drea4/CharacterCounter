c1c53bb28eb1853c856bc0efc1ee9dd8e1dbcdd3
4drea4 <bakerand@outlook.com>
2025-12-07 10:50:03 -0600

    very junky file cleaning up things I dont understand, or may not need to be here

--- src/components/CharacterCounter/CharacterCounter.tsx
+++ src/components/CharacterCounter/CharacterCounter.tsx
@@ -1,32 +1,22 @@
 //parent component because it will render stats display and input
 import React, {useState} from 'react';
-import type { CharacterCounterProps, StatsDisplayProps } from '../../types/index.ts';
+import type { CharacterCounterProps} from '../../types/index.ts';
+import type { StatsDisplay} from '../../types/index.ts';
 import type { TextInput} from '../TextInput/TextInput.tsx';
 
 
 
 export const CharacterCounter: React.FC<CharacterCounterProps> = ({
 
-    const [stats, setStats] = useState({ characterCount: 0, wordCount:0, readingTime:0
  });
  
-    //callback function 
-    const inputText: React.FC<TextInput>{
-    const [input, setInput] = useState({
+    //state
+   const [input, setInput] = useState('');
+
+ return (
+)
 
 
- })}
-
- return (<div>
-    <p> Word Count: {stats.wordCount}</p>
-    <p> Character Count: {stats.characterCount}</p>
-    <p> Estimated Reading time : {stats.readingTime}</p>
-    <input type="text" value = {}
-
- </div>)
-
-}
-export default
 
 //character counter is the parent
 //character counter has to pass input and stats in a callback
